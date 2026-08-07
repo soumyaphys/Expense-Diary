4f4e151a03a79d9b280f82a1a9bd51060dcb69e7
soumyaphys <csoumya552@gmail.com>
2026-08-08 00:43:10 +0530

    Add files via upload
    Expense Diary v1.1 - PWA Support

--- js/script.js
+++ js/script.js
@@ -197,3 +197,18 @@ resetMonthButton.onclick = function(){
     }
 
 }
+
+if ('serviceWorker' in navigator) {
+
+    window.addEventListener('load', function(){
+
+        navigator.serviceWorker.register('./service-worker.js')
+        .then(function(){
+
+            console.log("Service Worker Registered");
+
+        });
+
+    });
+
+}
