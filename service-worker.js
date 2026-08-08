765bd1d29a30b12cb5172baee90c7ef229af3205
soumyaphys <csoumya552@gmail.com>
2026-08-08 09:41:15 +0530

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "expense-diary-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./js/script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", function(event) {
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+
+            return Promise.all(
+
+                cacheNames
+                    .filter(function(name) {
+                        return name !== CACHE_NAME;
+                    })
+                    .map(function(name) {
+                        return caches.delete(name);
+                    })
+
+            );
+
+        })
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", function(event) {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(function(response) {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
